c18f62d430f96cc4d5e45d3cd974f928eb55f994
Kyle Gendreau <kyle@Kyles-MacBook-Pro.local>
2021-06-28 05:58:24 -0500

    value is now an optional

--- include/Node.h
+++ include/Node.h
@@ -5,13 +5,15 @@
 #ifndef SIMPLETRIETEMPLATE_NODE_H
 #define SIMPLETRIETEMPLATE_NODE_H
 
-#include <forward_list>
 #include <vector>
+#include <optional>
 
 template<typename K, typename T, uint32_t S>
 struct Node {
 
-    explicit Node(Node* parentPtr = nullptr, K key = K(), std::forward_list<T> value = std::forward_list<T>());
+    explicit Node(Node* parent = nullptr);
+
+    explicit Node(K key, Node* parent = nullptr);
 
     explicit Node(const Node& rhs);
 
@@ -20,20 +22,19 @@ struct Node {
     void swap(Node &rhs);
 
     Node& operator=(const Node& rhs);
-
     Node& operator=(const Node&& rhs);
 
     bool operator==(const Node& rhs);
 
     bool operator!=(const Node& rhs);
 
-    Node* parent;
+    K key_;
 
-    K key;
+    Node* parent_;
 
-    std::forward_list<T> value;
+    std::optional<T> value_;
 
-    std::vector<std::unique_ptr<Node<K,T,S>>> child;
+    std::vector<std::unique_ptr<Node<K,T,S>>> child_;
 
 };
 
--- src/Node.cpp
+++ src/Node.cpp
@@ -8,15 +8,18 @@
 #include "../include/Node.h"
 
 template<typename K, typename T, uint32_t S>
-Node<K, T, S>::Node(Node* parentPtr, K key, std::forward_list<T> value) : parent(parentPtr), key(key), value(value), child(std::vector<std::unique_ptr<Node<K,T,S>>>(S)) {
+Node<K, T, S>::Node(Node *parent) : parent_(parent), value_(std::nullopt), child_(std::vector<std::unique_ptr<Node<K,T,S>>>(S)) {}
+
+template<typename K, typename T, uint32_t S>
+Node<K, T, S>::Node(K key, Node* parent) : key_(key), parent_(parent), value_(std::nullopt), child_(std::vector<std::unique_ptr<Node<K,T,S>>>(S)) {
 }
 
 template<typename K, typename T, uint32_t S>
-Node<K, T, S>::Node(const Node &rhs) : parent(nullptr), key(rhs.key), value(rhs.value), child(std::vector<std::unique_ptr<Node<K,T,S>>>(S)) {
+Node<K, T, S>::Node(const Node &rhs) : parent_(nullptr), key_(rhs.key_), value_(rhs.value_), child_(std::vector<std::unique_ptr<Node<K,T,S>>>(S)) {
     for (uint32_t i(0); i < S; ++i) {
         if (rhs.child.at(i).get() != nullptr) {
-            child.at(i) = std::make_unique<Node<K, T, S>>(*(rhs.child.at(i)));
-            child.at(i)->parent = this;
+            child_.at(i) = std::make_unique<Node<K, T, S>>(*(rhs.child.at(i)));
+            child_.at(i)->parent = this;
         }
     }
 }
@@ -24,18 +27,18 @@ Node<K, T, S>::Node(const Node &rhs) : parent(nullptr), key(rhs.key), value(rhs.
 template<typename K, typename T, uint32_t S>
 void Node<K, T, S>::swap(Node &rhs) {
     if (this != &rhs) {
-        std::swap(parent, rhs.parent);
-        std::swap(key, rhs.key);
-        std::swap(value, rhs.value);
-        std::swap(child, rhs.child);
+        std::swap(parent_, rhs.parent_);
+        std::swap(key_, rhs.key_);
+        std::swap(value_, rhs.value_);
+        std::swap(child_, rhs.child_);
     }
 }
 
 template<typename K, typename T, uint32_t S>
 Node<K,T,S> &Node<K, T, S>::operator=(const Node &rhs) {
     if (this != &rhs) {
-        Node* ph(new Node<K,T,S>(rhs));
-        swap(*ph);
+        Node ph(rhs);
+        swap(ph);
     }
     return *this;
 }
@@ -47,12 +50,13 @@ Node<K,T,S> &Node<K, T, S>::operator=(const Node &&rhs) {
 
 template<typename K, typename T, uint32_t S>
 bool Node<K, T, S>::operator==(const Node &rhs) {
+    //todo should this include comparing child?
     if (this == &rhs)
         return true;
 
-    if ((key == rhs.key) && (value == rhs.value)) {
+    if ((key_ == rhs.key_) && (value_ == rhs.value_)) {
         for (uint32_t i(0); i < S; ++i) {
-            if (child.at(i).get() == nullptr) {
+            if (child_.at(i).get() == nullptr) {
                 if (rhs.child.at(i).get() != nullptr)
                     return false;
             }
@@ -60,7 +64,7 @@ bool Node<K, T, S>::operator==(const Node &rhs) {
                 if (rhs.child.at(i).get() == nullptr)
                     return false;
 
-                if (*(child.at(i)) != *(rhs.child.at(i)))
+                if (*(child_.at(i)) != *(rhs.child.at(i)))
                     return false;
             }
         }
@@ -73,6 +77,5 @@ template<typename K, typename T, uint32_t S>
 bool Node<K, T, S>::operator!=(const Node &rhs) {
     return !(*this == rhs);
 }
-
 
 #endif // SIMPLETRIETEMPLATE_NODE_CPP
