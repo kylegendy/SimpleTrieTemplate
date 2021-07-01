1f23d9fbdcce6f1f9f359428d6c0d54fccb65a9d
Kyle Gendreau <kyle@Kyles-MacBook-Pro.local>
2021-07-01 15:36:03 -0500

    created a radix default

--- include/DefaultParameters.h
+++ include/DefaultParameters.h
@@ -8,39 +8,60 @@
 #include "SimpleTrieTemplate.h"
 
 // assume all upper-case letters
-class RadixIndexer {
+class Radix_Indexer {
 public:
-    int32_t operator()(const std::string& input, const Node<std::string,bool,26>* node) {
-        // if node points to nullptr, then a new node must be created
-        if (node == nullptr)
-            return -2;
+    // called when not inserting
+    int32_t operator()(std::string& input, const Node<std::string,bool,26>* node) {
+        // assume never nullptr
+        int16_t diff(findDeviation(input,node->key_));
 
-        // cannot insert empty string
-        if (input.length() == 0)
-            throw std::logic_error("\ninserting an empty string\n");
-
-        // find where strings deviate
-        int16_t dif(findDeviation(input,node->key));
-
-        if (dif == input.length())
-            return (dif == node->key.length()) ? -1 : -2; // checks for equivalence
-        else if (dif == node->key.length())
-            return findChildIndex(input.at(dif)); // move on to next node
-        else if (dif == 0) {
-            if (node->key.length() == 0) {
-                // if this is the root, then move on to next node
-                if (node->parent == nullptr)
-                    return findChildIndex(input.at(dif));
-
-                // this should never happen otherwise!!!
-                throw std::logic_error("\na non-root node holds empty string");
-            }
-            // else it is most likely a trie that holds a letter as the starting node, and must be modified
-            return -3;
+        if (diff == input.size()) { // if found all of key
+            input = "";
+            // if node key is longer
+            return (diff != node->key_.size()) ? -2 : -1;
+        }
+        else if (diff == node->key_.size()) { // move on to next child
+            input = input.substr(diff);
+            return findChildIndex(input.at(0));
         }
 
-        // else dif is somewhere in the middle of BOTH strings, and thus the node's key and children must be modded
-        return -2;
+        throw std::logic_error("not sure what heppened here lmao\n");
+    }
+
+    // called when inserting
+    int32_t operator()(std::string& input, Node<std::string,bool,26>* &node) {
+        // if nullptr, found your node!!
+        if (node == nullptr) {
+            node = new Node<std::string,bool,26>(input);
+            input = "";
+            return -1;
+        }
+
+        int16_t diff(findDeviation(input,node->key_)); // find the first index of difference between strings
+
+        if (diff == 0) {
+            if (node->key_.empty())
+                input = input.substr(diff);
+            else // legit just different beginning letters... todo assume first child node of root?
+                breakUpNode(*node,diff);
+        }
+        else if (diff == input.size()) { // if found all of key
+            input = "";
+            if (diff != node->key_.size()) { // node key is larger, must break up node
+                breakUpNode(*node, diff);
+            }
+            // if node key is longer
+            return -1;
+        }
+        else if (diff == node->key_.size()) { // node key is smaller than input, move on to next child
+            input = input.substr(diff);
+        }
+        else { // the difference is in the middle of both strings!!! tough
+            breakUpNode(*node, diff);
+            input = input.substr(diff);
+        }
+
+        return findChildIndex(input.at(0));
     }
 
     // returns the index at deviation
@@ -56,129 +77,101 @@ public:
     static int16_t findChildIndex(const char16_t& letter) {
         return letter - 'A';
     }
-};
 
-class RadixModifier {
-public:
-    std::string operator()(std::string& input, Node<std::string,bool,26>* &curNode, const int32_t &signal) {
-        if (signal > -1) {
-            // else it signals a child location, and thus part of input string fully overlaps node string
-            int16_t dif(RadixIndexer::findDeviation(input,curNode->key));
-            return input.substr(dif);
-        }
-        else if (signal == -2) {
-            // treat for nullptr
-            if (curNode == nullptr) {
-                curNode = new Node<std::string,bool,26>(nullptr,input);
-                return input;
-            }
-            else {
-                // A) input string partially overlaps node
-                // B) part of input string partially overlaps node
-                int16_t dif(RadixIndexer::findDeviation(input,curNode->key));
+    static void breakUpNode(Node<std::string,bool,26>& node, int16_t diff) {
+        Node<std::string,bool,26> *ph_copy(new Node(node)), *def(new Node(node.parent_)); // make a copy and default
 
-                Node<std::string,bool,26>* duplicate(new Node<std::string,bool,26>(*curNode));
-                duplicate->key = duplicate->key.substr(dif);
-                curNode->key = curNode->key.substr(0,dif);
-                curNode->value.clear();
-                for (uint32_t i(0); i<26; ++i) {
-                    curNode->child.at(i).reset();
-                }
-                int16_t childIndex(RadixIndexer::findChildIndex(duplicate->key.at(0)));
-                curNode->child[childIndex].reset(duplicate);
-                duplicate->parent = curNode;
+        node.swap(*def); // give default vals
+        node.key_ = ph_copy->key_.substr(0,diff); // reassign with prefix of key
+        ph_copy->key_ = ph_copy->key_.substr(diff); // reassign with postfix of key
 
-                return input;
-            }
-        }
-        else if ( signal == -1) {
-            return "";
-        }
-        else {
-            handleThree(curNode);
-            return input;
-        }
-    }
-
-    static void handleThree(Node<std::string,bool,26>* &curNode) {
-        assert(curNode->parent == nullptr);
-
-        // create new root node with empty string
-        Node<std::string,bool,26>* prevNode(new Node<std::string,bool,26>(*curNode));
-        curNode->key = "";
-        curNode->value.clear();
-        for (uint32_t i(0); i<26; ++i) {
-            curNode->child.at(i).reset();
-        }
-        curNode->child.at(RadixIndexer::findChildIndex(prevNode->key.at(0))).reset(prevNode);
-        prevNode->parent = curNode;
+        node.child_.at(findChildIndex(ph_copy->key_.at(0))).reset(ph_copy);
+        ph_copy->parent_ = &node;
     }
 };
 
-class RadixEraser {
+class Radix_Eraser {
 public:
-    void operator()(Iterator<std::string,bool,26> &ancestor, Iterator<std::string,bool,26> &descendant, SimpleTrieTemplate<std::string,bool,26,RadixIndexer,RadixModifier,RadixEraser> &trie) {
-        if (ancestor->child.empty() || descendant == ancestor) {
-            return;
+    void operator()(Node<std::string,bool,26> &ancestor, Node<std::string,bool,26> &descendant, SimpleTrieTemplate<std::string,bool,26,Radix_Indexer,Radix_Eraser> &trie) {
+        assert(descendant != *trie.end().get());
+        uint16_t cnt(childCount(descendant));
+        if (cnt == 0) { // if child empty
+            handleDelete(ancestor,descendant,trie);
+
+            if (descendant != *trie.end().get()) // handle descendant now that you've moved it, consider it the parent
+                handleCompacting(ancestor,descendant,trie);
         }
-        else {
-            descendant->value.clear();
-            uint16_t numChild(getNumChild(descendant));
-            // if child empty
-            if (numChild == 0) {
-                handleAncestors(descendant.moveUp(), trie);
-            }
-            // if only one child
-            else if (numChild == 1) {
-                handleCompact(descendant);
-                handleCompact(descendant.moveUp());
-            }
-            // if multiple children
-            else {
-                handleCompact(descendant.moveUp());
-            }
+        else { // has children
+
+            // empty out value so compacting will work
+            descendant.value_ = std::nullopt;
+
+            // handle potential children
+            handleCompacting(ancestor,descendant,trie);
+
+            if (!descendant.value_ && childCount(descendant) < 2)
+                handleDelete(ancestor,descendant,trie);
+
+            // handle parent now that you've changed descendant
+            if (descendant != *trie.end().get())
+                handleCompacting(ancestor,*descendant.parent_,trie);
         }
     }
 
-    static uint16_t getNumChild(Iterator<std::string,bool,26> &descendant) {
-        uint16_t num(0);
-        for (uint16_t i(0); i < 26; ++i) {
-            if (descendant->child.at(i).get() != nullptr)
-                ++num;
+    static uint32_t findChildsIndex(Node<std::string,bool,26> &child) {
+        assert(child.parent_ != nullptr);
+        Node<std::string,bool,26>* parptr(child.parent_);
+        for (uint32_t i(0); i < 26; ++i) {
+            if (parptr->child_.at(i).get() == &child)
+                return i;
         }
-        return num;
+        throw std::domain_error("child's index not found -- potential linking problem");
     }
 
-    static void handleAncestors(Iterator<std::string,bool,26> &it,SimpleTrieTemplate<std::string,bool,26,RadixIndexer,RadixModifier,RadixEraser> &trie) { // todo this shit is fucked and breaks on erase tests
-        if (it->parent == nullptr) {
-            trie.clear();
-        }
-        else {
-            it.moveUp();
-            it->child.at(it.getIndex()).reset();
-            if (it->value.empty()) {
-                if (getNumChild(it) == 0) {
-                    handleAncestors(it,trie);
+    static void handleCompacting(Node<std::string,bool,26>& ancestor, Node<std::string,bool,26>& parent, SimpleTrieTemplate<std::string,bool,26,Radix_Indexer,Radix_Eraser> &trie) {
+        // if doesn't have assigned value
+        if (!parent.value_) {
+            uint16_t cnt(childCount(parent));
+
+            if (cnt == 0) { // if child empty
+                // rollback delete and compact delete
+                handleDelete(ancestor,parent,trie);
+                handleCompacting(ancestor,parent,trie);
+            }
+            else if (cnt == 1) { // if only one child
+                for (uint16_t i(0); i < 26; ++i) {
+                    if (parent.child_.at(i) != nullptr) {
+
+                        // merge
+                        Node<std::string,bool,26>* ph_copy(new Node<std::string,bool,26>(*parent.child_.at(i).get()));
+                        ph_copy->key_ = parent.key_ + ph_copy->key_;
+                        ph_copy->parent_ = parent.parent_;
+
+                        parent.swap(*ph_copy);
+                        break;
+                    }
                 }
             }
         }
     }
 
-    static void handleCompact(Iterator<std::string,bool,26> &ancestor) {
-        int16_t index(-1);
+    static uint16_t childCount(Node<std::string,bool,26>& node) {
+        uint16_t cnt;
         for (uint16_t i(0); i < 26; ++i) {
-            if (ancestor->child.at(i).get() != nullptr) {
-                if (index != -1)
-                    return;
-                else
-                    index = i;
-            }
+            if (node.child_.at(i) != nullptr)
+                ++cnt;
         }
-        std::string prevStr(ancestor->key);
-        Node<std::string,bool,26>* prevParent(ancestor->parent);
-        *ancestor = *(ancestor->child.at(index).get());
-        ancestor->key = prevStr + ancestor->key;
-        ancestor->parent = prevParent;
+        return cnt;
+    }
+
+    static void handleDelete(Node<std::string,bool,26> &ancestor, Node<std::string,bool,26> &descendant, SimpleTrieTemplate<std::string,bool,26,Radix_Indexer,Radix_Eraser> &trie) {
+        if (descendant != *trie.end().get()) {
+            uint16_t childIndex(findChildsIndex(descendant)); // find the index of descendant in parents child
+            if (ancestor == descendant)
+                ancestor = descendant.parent_;
+            descendant = descendant.parent_;
+            descendant.child_.at(childIndex).reset();
+        }
     }
 };
 
